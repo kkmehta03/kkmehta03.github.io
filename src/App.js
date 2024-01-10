4c3a62ffa63e26fb62578eeb78c16c06ec5e1b0a
mehtakk <mehtakk10@gmail.com>
2024-01-10 14:21:54 +0530

    correct the scene

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Spline from '@splinetool/react-spline';
 export default function App() {
   return (
     // <Spline scene="https://prod.spline.design/a0jpSfqsUR1UAqJu/scene.splinecode" />
-    <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />  );
-}
+    <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />}
 
 
