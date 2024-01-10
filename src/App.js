930da6058223e030409441e46df31a11ccb8ee6e
mehtakk <mehtakk10@gmail.com>
2024-01-10 14:17:41 +0530

    adding another random 3d model

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/a0jpSfqsUR1UAqJu/scene.splinecode" />
+    // <Spline scene="https://prod.spline.design/a0jpSfqsUR1UAqJu/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
   );
 }
 
