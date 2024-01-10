27affaa913aedc1a4fb8055305b457dc14599022
mehtakk <mehtakk10@gmail.com>
2024-01-10 15:56:05 +0530

    make the environment to become one touch orbit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <div style={{height: '100vh'}}>
+    <div style={{ height: '100vh' }}>
       {/* <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
       {/* <Spline width="100%" height="100%" scene="https://prod.spline.design/
       Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
-          <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
     </div>
 
   )
