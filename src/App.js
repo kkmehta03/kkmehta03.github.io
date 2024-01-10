bccbb065ec951088689355614bbf294fe01ae8de
mehtakk <mehtakk10@gmail.com>
2024-01-10 15:59:24 +0530

    make the view width also to be 100%

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: '100vh', width:'100wv' }}>
       {/* <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
       {/* <Spline width="100%" height="100%" scene="https://prod.spline.design/
       Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
