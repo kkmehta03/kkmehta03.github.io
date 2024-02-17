1883fe42f7c40a0d365ca09f68d87c0e32019224
mehtakk <mehtakk10@gmail.com>
2024-02-17 14:53:18 +0530

    just comitting the whole thing so it doesnt get lost

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import Spline from '@splinetool/react-spline';
-
+import logo from './kk-codes-logo.png';
+import './App.css';
 export default function App() {
   return (
-    <div style={{ height: '100vh', width:'100wv' }}>
-      {/* <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
-      {/* <Spline width="100%" height="100%" scene="https://prod.spline.design/
-      Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
-      <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Spline className = "Spline" scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
+    
+    // <div style={{ height: '100vh', width:'100wv' }}>
+      
+      
+    //   {/* <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
+    //   {/* <Spline width="100%" height="100%" scene="https://prod.spline.design/
+    //   Dl2RAeXjIDjgxq7m/scene.splinecode" /> */}
+    //   <Spline scene="https://prod.spline.design/Dl2RAeXjIDjgxq7m/scene.splinecode" />
+    // </div>
 
   )
   // <Spline scene="https://prod.spline.design/a0jpSfqsUR1UAqJu/scene.splinecode" />
